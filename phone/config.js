66f9b827f1066bd975dc6c78fa4c15f0928fcef8
Sean Lee <iseansay@gmail.com>
2014-05-09 00:18:16 +0800

    phone and sound

--- phone/config.js
+++ phone/config.js
@@ -5,6 +5,10 @@ module.exports = {
     { rootUrl: '/', dirName: './app' }
   ],
   clients: [
-    { ip: '127.0.0.1', appPort: 9001 }
+    { ip: '127.0.0.1', appPort: 9001 },
+    { ip: '127.0.0.1', appPort: 12000 },
+    { ip: '192.168.1.100', appPort: 12000 },
+    { ip: '192.168.1.101', appPort: 12000 },
+    { ip: '192.168.1.102', appPort: 12000 }    
   ]
 }
